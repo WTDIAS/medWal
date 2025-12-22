ab14ae6b63ca1560615ed9e28c93edc9b892ec8b
Waldir Tiago Dias <waldirtiagodias@gmail.com>
2025-12-22 10:19:45 -0300

    inclusão de dependencia e alteração no código para conseguir ler o .env

--- src/main/java/br/com/gigalike/medWal/MedWalApplication.java
+++ src/main/java/br/com/gigalike/medWal/MedWalApplication.java
@@ -1,10 +1,16 @@
 package br.com.gigalike.medWal;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedWalApplication {
 	public static void main(String[] args) {
+        // Carrega o arquivo .env da raiz
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Configura as propriedades do sistema para o Spring ler
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MedWalApplication.class, args);
 	}
 }
